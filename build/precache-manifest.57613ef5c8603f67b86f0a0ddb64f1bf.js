8c0dcc8eaae452624dd0496e709a345c56abe170
jeevansuriya <jeevansuriya.r@gmail.com>
2021-12-28 12:44:53 +0530

    commit

---
+++ build/precache-manifest.57613ef5c8603f67b86f0a0ddb64f1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3460b6fe9937025b6b2e0cad87c2570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7393ed22154aad48f508",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7393ed22154aad48f508",
+    "url": "/static/js/2.6d6a3a04.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6d6a3a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc45ab9dbffc9f768e69",
+    "url": "/static/js/main.ae1f0f7d.chunk.js"
+  },
+  {
+    "revision": "1d05d21cfb7c11100d5b",
+    "url": "/static/js/runtime-main.5be0474a.js"
+  }
+]);
